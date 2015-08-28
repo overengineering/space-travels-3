badf6f9e43a61bbb4aad7ca007572631579ddc93
Stefano Chiodino <LouderTH@gmail.com>
2015-08-28 21:51:22 +0100

    init

---
+++ desktop/src/com/draga/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.draga.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.draga.SpaceTravels3;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceTravels3(), config);
+	}
+}
---
+++ html/src/com/draga/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.draga.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.draga.SpaceTravels3;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SpaceTravels3();
+        }
+}
