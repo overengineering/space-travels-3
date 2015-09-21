879955d17d5201c1103a81d6ee2ae14474c76d1a
Stefano Chiodino <LouderTH@gmail.com>
2015-09-21 18:06:23 +0100

    seemingly working ship rotation

--- core/src/com/draga/Ship/ShipPhysicComponent.java
+++ core/src/com/draga/Ship/ShipPhysicComponent.java
@@ -10,11 +10,9 @@ import com.draga.event.GravityEvent;
 import com.google.common.eventbus.Subscribe;
 
 public class ShipPhysicComponent extends RectangularPhysicComponent {
-    private static final float MAX_SPEED = 100f;
-    private static final float MAX_ACCELERATION_SECOND = MAX_SPEED * 2f;
+    public static final int ROTATION_FORCE = 10000;
     private static final int SHIP_WIDTH = 10;
     private static final int SHIP_HEIGHT = 10;
-    private static final float ROTATION_PER_SECOND = 1000f;
 
     public ShipPhysicComponent(int x, int y) {
         super(x, y, SHIP_WIDTH, SHIP_HEIGHT, 1f, BodyDef.BodyType.DynamicBody);
@@ -29,17 +27,24 @@ public class ShipPhysicComponent extends RectangularPhysicComponent {
     /**
      * Rotate the ship towards the given vector smoothly
      *
-     * @param accelerometerForce The force of gravity on the device, should be capped to the constant Earth gravity
+     * @param accelerometerForce The force of gravity on the device, should be capped to the Earth gravity
      */
     private void rotateTo(Vector2 accelerometerForce, float elapsed) {
+        float nextAngle = body.getAngle() + body.getAngularVelocity();
         float directionAngle = accelerometerForce.angleRad();
 
-        float diffRotation = directionAngle - getAngle();
-        while (diffRotation < -180 * MathUtils.degreesToRadians)
+        float diffRotation = directionAngle - nextAngle;
+
+        // Brings the desired rotation between -180 and 180 degrees to find the closest way to turn.
+        // E.g.: rotates - 10 degrees instead of 350.
+        while (diffRotation < -180 * MathUtils.degreesToRadians) {
             diffRotation += 360 * MathUtils.degreesToRadians;
-        while (diffRotation > 180 * MathUtils.degreesToRadians)
+        }
+        while (diffRotation > 180 * MathUtils.degreesToRadians) {
             diffRotation -= 360 * MathUtils.degreesToRadians;
-        body.applyTorque(diffRotation * 100000 * elapsed, true);
+        }
+
+        body.applyAngularImpulse(diffRotation * ROTATION_FORCE * elapsed, true);
     }
 
     @Subscribe public void gravity(GravityEvent gravityEvent) {
