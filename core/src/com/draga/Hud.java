f44b885e8fd765966c5d2287ae4a43ca40b921ca
Stefano Chiodino <LouderTH@gmail.com>
2015-10-16 17:57:38 +0100

    use screen size and it's proportions instead of specifying it

--- core/src/com/draga/Hud.java
+++ core/src/com/draga/Hud.java
@@ -13,14 +13,6 @@ import com.google.common.eventbus.Subscribe;
 
 public class Hud implements Screen
 {
-    private static final float WORLD_HEIGHT                  = 1000f;
-    private static final float WORLD_WIDTH                   = 1000f;
-    private static final float FUEL_PROGRESS_BAR_HEIGHT      = WORLD_HEIGHT / 30f;
-    private static final float FUEL_PROGRESS_BAR_WIDTH       = WORLD_WIDTH / 3f;
-    @SuppressWarnings("SuspiciousNameCombination")
-    private static final float FUEL_PROGRESS_BAR_LEFT_MARGIN = FUEL_PROGRESS_BAR_HEIGHT;
-    @SuppressWarnings("SuspiciousNameCombination")
-    private static final float FUEL_PROGRESS_BAR_TOP_MARGIN  = FUEL_PROGRESS_BAR_LEFT_MARGIN;
     private Stage       stage;
     private ProgressBar fuelProgressBar;
 
@@ -28,8 +20,7 @@ public class Hud implements Screen
     {
         Constants.EVENT_BUS.register(this);
 
-        StretchViewport viewport = new StretchViewport(WORLD_WIDTH, WORLD_HEIGHT);
-        stage = new Stage(viewport);
+        stage = new Stage();
 
         fuelProgressBar = getFuelProgressBar();
         updateFuelProgressBar(Constants.MAX_FUEL);
@@ -39,9 +30,13 @@ public class Hud implements Screen
 
     private ProgressBar getFuelProgressBar()
     {
+        float height = stage.getHeight() / 30f;
+        float width = stage.getWidth() / 3f;
+        float margin = ((stage.getHeight()+stage.getWidth()) /2f) / 50f;
+
         Skin skin = new Skin();
         Pixmap pixmap = new Pixmap(
-            1, (int) Math.ceil(FUEL_PROGRESS_BAR_HEIGHT), Pixmap.Format.RGBA8888);
+            1, (int) Math.ceil(height), Pixmap.Format.RGBA8888);
         pixmap.setColor(Color.WHITE);
         pixmap.fill();
         skin.add("white", new Texture(pixmap));
@@ -52,10 +47,10 @@ public class Hud implements Screen
 
         ProgressBar fuelProgressBar = new ProgressBar(
             0, Constants.MAX_FUEL, Constants.MAX_FUEL / 1000f, false, fuelProgressBarStyle);
-        fuelProgressBar.setSize(FUEL_PROGRESS_BAR_WIDTH, FUEL_PROGRESS_BAR_HEIGHT);
+        fuelProgressBar.setSize(width, height);
         fuelProgressBar.setPosition(
-            FUEL_PROGRESS_BAR_LEFT_MARGIN,
-            WORLD_HEIGHT - FUEL_PROGRESS_BAR_TOP_MARGIN - FUEL_PROGRESS_BAR_HEIGHT);
+            margin,
+            stage.getHeight() - margin - height);
 
         return fuelProgressBar;
     }
