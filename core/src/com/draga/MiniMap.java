77d1042470c03b072ea94d5e2350c86eaadd2a35
Stefano Chiodino <LouderTH@gmail.com>
2015-11-24 18:55:08 +0000

    F5 mark II

--- core/src/com/draga/MiniMap.java
+++ core/src/com/draga/MiniMap.java
@@ -1,15 +1,14 @@
 package com.draga;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 
 public class MiniMap
 {
-    private static final float   MINIMAP_SCALE = 0.25f;
-    private static final OrthographicCamera orthographicCamera = new OrthographicCamera();
-    private static ShapeRenderer shapeRenderer = new ShapeRenderer();
+    private static final float              MINIMAP_SCALE      = 0.25f;
+    private static final OrthographicCamera orthographicCamera = new OrthographicCamera(300, 150);
+    private static       ShapeRenderer      shapeRenderer      = new ShapeRenderer();
 
     static
     {
@@ -29,29 +28,33 @@ public class MiniMap
             newCameraBounds.merge(mergeRectangle);
         }
 
-        float screenAspectRatio = (float) 300 / 150;
-
-        if (newCameraBounds.getAspectRatio() > screenAspectRatio)
-        {
-            newCameraBounds.height *= newCameraBounds.getAspectRatio() / screenAspectRatio;
-        }
-        else if (newCameraBounds.getAspectRatio() < screenAspectRatio)
-        {
-            newCameraBounds.width *= screenAspectRatio / newCameraBounds.getAspectRatio();
-        }
+        //        float screenAspectRatio = (float) 300 / 150;
         //
-//        newCameraBounds.width = 300;
-//        newCameraBounds.height = 150;
+        //        if (newCameraBounds.getAspectRatio() > screenAspectRatio)
+        //        {
+        //            newCameraBounds.height *= newCameraBounds.getAspectRatio() / screenAspectRatio;
+        //        }
+        //        else if (newCameraBounds.getAspectRatio() < screenAspectRatio)
+        //        {
+        //            newCameraBounds.width *= screenAspectRatio / newCameraBounds.getAspectRatio();
+        //        }
+        //        //
+        //        newCameraBounds.width = 300;
+        //        newCameraBounds.height = 150;
 
 
+        //        if (newCameraBounds.x - (newCameraBounds.width / 2f))
 
-        orthographicCamera.setToOrtho(false, newCameraBounds.width, newCameraBounds.height);
+        //        orthographicCamera.setToOrtho(false, newCameraBounds.width, newCameraBounds.height);
+        //        orthographicCamera.setToOrtho(false, 300, 150);
+        orthographicCamera.zoom =
+            Math.max(newCameraBounds.width / 300, newCameraBounds.height / 150);
         orthographicCamera.position.set(
-            newCameraBounds.x,
-            newCameraBounds.y,
+            newCameraBounds.x + 150,
+            newCameraBounds.y + 75,
             0);
         //        orthographicCamera.position.set(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2,0);
-        orthographicCamera.zoom = 1 / MINIMAP_SCALE;
+        //        orthographicCamera.zoom = 1 / MINIMAP_SCALE;
         orthographicCamera.update();
         shapeRenderer.setProjectionMatrix(orthographicCamera.combined);
     }
