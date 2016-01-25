e551b91e1a987e0c5fd19c80a0064b4f1c9cb966
Stefano Chiodino <LouderTH@gmail.com>
2016-01-25 20:04:20 +0000

    credit screen to single column

--- core/src/com/draga/spaceTravels3/screen/CreditsScreen.java
+++ core/src/com/draga/spaceTravels3/screen/CreditsScreen.java
@@ -46,40 +46,58 @@ public class CreditsScreen extends Screen
     {
         Table table = UIManager.getDefaultTable();
 
-        table.add("Developers");
-        table.add("Stefano Chiodino\r\n"
-            + "Lee Richardson");
+        table
+            .add("Developers", "large")
+            .row();
+        table
+            .add("Stefano Chiodino\r\n"
+                + "Lee Richardson")
+            .row();
 
-        table.row();
-        table.add();
+        table
+            .add()
+            .row();
 
-        table.row();
-        table.add("Earth\r\nJupiter\r\nVenus\r\nMars");
-        table.add("NASA");
+        table
+            .add("Earth, Jupiter, Venus, Mars", "large")
+            .row();
+        table
+            .add("NASA")
+            .row();
 
-        table.row();
-        table.add();
+        table
+            .add()
+            .row();
 
-        table.row();
-        table.add("Star\r\nicon");
-        table.add("Author: I, Ssolbergj\r\n"
-            + "Changes: saturation, transparency\r\n"
-            + "Licence: CC BY 3.0");
+        table
+            .add("Star icon", "large")
+            .row();
+        table
+            .add("Author: I, Ssolbergj\r\n"
+                + "Changes: saturation, transparency\r\n"
+                + "Licence: CC BY 3.0")
+            .row();
 
-        table.row();
-        table.add();
+        table
+            .add()
+            .row();
 
-        table.row();
-        table.add("Explosion\r\nsound");
-        table.add("Author: Blender Foundation\r\n"
-            + "Changes: normalised\r\n"
-            + "Licence: CC BY 3.0");
+        table
+            .add("Explosion sound", "large")
+            .row();
+        table
+            .add("Author: Blender Foundation\r\n"
+                + "Changes: normalised\r\n"
+                + "Licence: CC BY 3.0")
+            .row();
 
-        table.row();
-        table.add();
+        table
+            .add()
+            .row();
 
-        table.row();
-        table.add("Thruster");
+        table
+            .add("Thruster", "large")
+            .row();
         Label thrusterLabel = new Label(
             "WrathGames Studio\r\n"
                 + "http://wrathgames.com/blog\r\n"
@@ -93,13 +111,17 @@ public class CreditsScreen extends Screen
                 Gdx.net.openURI("http://wrathgames.com/blog");
             }
         });
-        table.add(thrusterLabel);
+        table
+            .add(thrusterLabel)
+            .row();
 
-        table.row();
-        table.add();
+        table
+            .add()
+            .row();
 
-        table.row();
-        table.add("Thruster\r\nsound");
+        table
+            .add("Thruster sound", "large")
+            .row();
         Label thrusterSoundLabel = new Label(
             "Fire Loop by Iwan 'qubodup' Gabovitch\r\n"
                 + "http://opengameart.org/users/qubodup\r\n"
@@ -113,13 +135,17 @@ public class CreditsScreen extends Screen
                 Gdx.net.openURI("http://opengameart.org/users/qubodup");
             }
         });
-        table.add(thrusterSoundLabel);
+        table
+            .add(thrusterSoundLabel)
+            .row();
 
-        table.row();
-        table.add();
+        table
+            .add()
+            .row();
 
-        table.row();
-        table.add("Licence\r\nlinks");
+        table
+            .add("Licence links", "large")
+            .row();
         Label ccby30Label = new Label(
             "Attribution 3.0 Unported (CC BY 3.0)\r\n"
                 + "https://creativecommons.org/licenses/by/3.0/",
@@ -132,7 +158,9 @@ public class CreditsScreen extends Screen
                 Gdx.net.openURI("https://creativecommons.org/licenses/by/3.0/");
             }
         });
-        table.add(ccby30Label);
+        table
+            .add(ccby30Label)
+            .row();
 
 
         ScrollPane scrollPane = new ScrollPane(table, UIManager.skin);
