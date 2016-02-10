829d40ceb0d46184fc0818dcbddd15b82d03b2b1
Stefano Chiodino <LouderTH@gmail.com>
2016-02-10 18:07:14 +0000

    fix #132 score in winscreen

--- core/src/com/draga/spaceTravels3/event/WinEvent.java
+++ core/src/com/draga/spaceTravels3/event/WinEvent.java
@@ -1,5 +1,15 @@
 package com.draga.spaceTravels3.event;
 
+import com.draga.spaceTravels3.Score;
+
 public class WinEvent
 {
+    public Score   score;
+    public Integer previousBestScore;
+
+    public WinEvent(Score score, Integer previousBestScore)
+    {
+        this.score = score;
+        this.previousBestScore = previousBestScore;
+    }
 }
